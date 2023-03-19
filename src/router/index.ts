7d467476c7b31f89ffffba023b93eeb432aa0b31
271757749@qq.com <271757749@qq.com>
2023-03-19 14:17:33 +0800

    add husky

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,25 @@
-import {createRouter ,createWebHistory} from 'vue-router'
-import Home from '@/pages/HomePage.vue'
-import About from '@/pages/AboutPage.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/pages/HomePage.vue";
+import About from "@/pages/AboutPage.vue";
 
 const routes = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            redirect: '/home'
-        },
-        {
-            path: '/home',
-            name: 'home',
-            component: Home,
-        }
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      redirect: "/home",
+    },
+    {
+      path: "/home",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About,
+    },
+  ],
+});
 
-export default routes
+export default routes;
